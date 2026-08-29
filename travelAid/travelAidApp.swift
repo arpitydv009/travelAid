d8df624667806ebe46d4f0372045f34989e7e29d
arpit0601 <66950445+arpit0601@users.noreply.github.com>
2026-08-29 11:19:35 +0530

    Initial Commit

---
+++ travelAid/travelAidApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelAidApp.swift
+//  travelAid
+//
+//  Created by arpit on 29/08/26.
+//
+
+import SwiftUI
+
+@main
+struct travelAidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
